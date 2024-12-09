923f201e6de14901799ba777d40cf8244b851402
유다한 (KT협력사) <91348450@ktfriend.com>
2024-12-09 06:48:46 +0000

    init

---
+++ src/test/java/com/memories/memoriesapi/MemoriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memories.memoriesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
